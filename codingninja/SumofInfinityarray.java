2b59d522ef96989c67f68a8b39f31f5d31c46706
KAPILSHARMA239 <kapil1714.cse18@chitkara.edu.in>
2021-09-09 13:04:38 +0530

    initial commit

---
+++ codingninja/SumofInfinityarray.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Solution {
+private static int mod = (int) 1e9 + 7;
+    public static List<Integer> sumInRanges(int[] arr, int n, List<List<Long>> queries, int q) {
+ List <Integer>out=new ArrayList<Integer>();
+   
+       
+       
+    
+ for(int i=0;i<q;i++)
+ {
+     int  sum=0;
+     
+ long l=(queries.get(i)).get(0)-1;
+ long r=(queries.get(i)).get(1)-1;
+     for(long j=l;j<=r;j++)
+     {
+        int index=(int)j%n;
+         sum=(sum+arr[index])%mod;
+         
+     }
+ sum%=mod;
+     out.add(sum);
+ 
+              
+ }
+     
+   
+        
+return out;
+    }
+   
+}
